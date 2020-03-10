8f2cb61146699ddbbc2c46e1caedb112353324fa
Edgar <edgar@byneapp.com>
2020-03-10 17:05:05 +0700

    create router and route names

--- lib/ui/screens/launcher/launcher_page.dart
+++ lib/ui/screens/launcher/launcher_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LauncherPage extends StatelessWidget {
+  static const String routeName = 'launcher';
   @override
   Widget build(BuildContext context) {
     return Container();
