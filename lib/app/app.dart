1e67fe606ed1f7b88d3fb6b6dde8a293c2f06a74
Edgar <edgar@byneapp.com>
2020-03-10 16:58:56 +0700

    create app :beers:

---
+++ lib/app/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Covid19App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      title: '',
+    );
+  }
+}
