1fc0a61375504e3a1066c4aa5930e5ab93bde281
Edgar <edgar@byneapp.com>
2020-03-10 17:11:25 +0700

    add routes to app :beers:

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,5 @@
+import 'package:covid19_app_fluter/app/router.dart';
+import 'package:covid19_app_fluter/ui/screens/launcher/launcher_page.dart';
 import 'package:flutter/material.dart';
 
 class Covid19App extends StatelessWidget {
@@ -6,6 +8,8 @@ class Covid19App extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(),
       title: '',
+      onGenerateRoute: AppRouter.generateRoute,
+      initialRoute: LauncherPage.routeName,
     );
   }
 }
