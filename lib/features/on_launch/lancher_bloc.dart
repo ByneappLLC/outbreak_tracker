7ca4c486c793f249e13fa21fa77fab6d41384e3c
Edgar <edgar@byneapp.com>
2020-03-11 14:56:46 +0700

    begin intialize

--- lib/features/on_launch/lancher_bloc.dart
+++ lib/features/on_launch/lancher_bloc.dart
@@ -1,6 +1,12 @@
 import 'package:outbreak_tracker/core/base_bloc.dart';
 
 class LauncherBloc extends BaseBloc {
+  LauncherBloc() {
+    _onInit();
+  }
+
+  _onInit() {}
+
   @override
   void dispose() {
     // TODO: implement dispose
--- lib/ui/screens/launcher/launcher_page.dart
+++ lib/ui/screens/launcher/launcher_page.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:outbreak_tracker/core/base_bloc.dart';
+import 'package:outbreak_tracker/di/di_instance.dart';
+import 'package:outbreak_tracker/features/on_launch/lancher_bloc.dart';
 
 class LauncherPage extends StatelessWidget {
   static const String routeName = 'launcher';
 
   static PageRoute newInstance() {
-    return MaterialPageRoute(builder: (_) => LauncherPage());
+    return MaterialPageRoute(
+        builder: (_) =>
+            BlocProvider(child: LauncherPage(), bloc: sl<LauncherBloc>()));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
