ee4b535d28c8f1eca5524c3775f2298496bb85d6
Edgar <edgar@byneapp.com>
2020-03-13 17:57:17 +0700

    add radius

--- lib/ui/screens/overview/widgets/top_countries.dart
+++ lib/ui/screens/overview/widgets/top_countries.dart
@@ -5,14 +5,9 @@ class TopCountries extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 10),
-      decoration:
-          BoxDecoration(color: Color.fromRGBO(29, 32, 38, 1.0), boxShadow: [
-        BoxShadow(
-            color: Colors.white70.withOpacity(0.3),
-            offset: Offset(1, 2),
-            spreadRadius: 3,
-            blurRadius: 10)
-      ]),
+      decoration: BoxDecoration(
+          color: Color.fromRGBO(29, 32, 38, 1.0),
+          borderRadius: BorderRadius.all(Radius.circular(8))),
       height: 250,
     );
   }
